634dcac4b637716e35b57509d4e96e0bff80e181
Michael Post <michael@egotribe.nl>
2024-01-14 22:30:38 +0100

    adds github

--- client/src/Contact/Contact.js
+++ client/src/Contact/Contact.js
@@ -3,6 +3,7 @@ import {
   faLocationDot,
   faPhone,
 } from "@fortawesome/free-solid-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import Heading from "../Components/Heading/Heading";
@@ -14,17 +15,23 @@ const Contact = () => {
     <section className="contact">
       <Heading title="Contact" />
       <div>
-        <div className="inline">
+        <div className="inline v-center">
           <FontAwesomeIcon icon={faPhone} className="icon" />
           <p>+ 31 6 39 77 75 55</p>
         </div>
-        <div className="inline">
+        <div className="inline v-center">
           <FontAwesomeIcon icon={faAt} className="icon" />
           <p>
             <a href="mailto:hallo@michaelpost.nl">hallo@michaelpost.nl</a>
           </p>
         </div>
-        <div className="inline">
+        <div className="inline v-center">
+          <FontAwesomeIcon icon={faGithub} className="icon" />
+          <p>
+            <a href="https://github.com/mchlpst">mchlpst</a>
+          </p>
+        </div>
+        <div className="inline v-center">
           <FontAwesomeIcon icon={faLocationDot} className="icon" />
           <p>
             Laplacestraat 57-H
